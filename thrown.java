8db1a1c9a78131e93019ff2f566cc19789409463
manojmahi90526 <23hr1a3012@mtieat.org>
2024-12-14 16:07:05 +0530

    Add files via upload

---
+++ thrown.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class throw_block2 {
+    public static void a(int a,int b){
+        if(b==0){
+            throw new ArithmeticException("can't devide");
+        }
+        System.out.println(a/b);
+    }
+    public static void main (String args[]){
+        try{
+            a(10,0);
+        }catch(ArithmeticException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ throws.java
@@ -0,0 +1,9 @@
+class Throws
+{
+    public static void main(String args[])throws ArithmeticException
+    {
+        int a=10;
+        int b=10;
+        System.out.println(a/b);
+    }
+}
